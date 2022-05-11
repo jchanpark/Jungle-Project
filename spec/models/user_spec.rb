8b45ac3baf6dd1410d6784a1180603f223fcc1c3
jchanpark <junpark4936@gamil.com>
2022-05-10 20:59:50 -0400

    fix discrepancies between DB and models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,72 +14,71 @@ RSpec.describe User, type: :model do
       )
       expect(@user).to be_valid
     end
+
+    it 'should not be valid without a first name' do
+      @user = User.new(
+        first_name: nil,
+        last_name: 'Last',
+        email: 'lighthout@lab.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid without a last name' do
+      @user = User.new(
+        first_name: 'First',
+        last_name: nil,
+        email: 'lighthout@lab.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid without an email' do
+      @user = User.new(
+        first_name: 'First',
+        last_name: 'Last',
+        email: nil,
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid without a password' do
+      @user = User.new(
+        first_name: 'First',
+        last_name: 'Last',
+        email: 'lighthout@lab.com',
+        password: nil,
+        password_confirmation: 'password'
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid without a password confirmation' do
+      @user = User.new(
+        first_name: 'First',
+        last_name: 'Last',
+        email: 'lighthout@lab.com',
+        password: 'password',
+        password_confirmation: nil
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it 'should not be valid if a password conirmation is different from the password' do
+      @user = User.new(
+        first_name: 'First',
+        last_name: 'Last',
+        email: 'lighthout@lab.com',
+        password: 'password',
+        password_confirmation: 'different'
+      )
+      expect(@user).to_not be_valid
+    end
   end
 end
-    # xit 'should not be valid without a first name' do
-    #   @user = User.new(
-    #     first_name: nil,
-    #     last_name: 'Last',
-    #     email: 'lighthout@lab.com',
-    #     password: 'password',
-    #     password_confirmation: 'password'
-    #   )
-    #   expect(@user).to_not be_valid
-    # end
-
-    # xit 'should not be valid without a last name' do
-    #   @user = User.new(
-    #     first_name: 'First',
-    #     last_name: nil,
-    #     email: 'lighthout@lab.com',
-    #     password: 'password',
-    #     password_confirmation: 'password'
-    #   )
-    #   expect(@user).to_not be_valids
-    # end
-
-    # xit 'should not be valid without an email' do
-    #   @user = User.new(
-    #     first_name: 'First',
-    #     last_name: 'Last',
-    #     email: nil,
-    #     password: 'password',
-    #     password_confirmation: 'password'
-    #   )
-    #   expect(@user).to_not be_valid
-    # end
-
-    # xit 'should not be valid without a password' do
-    #   @user = User.new(
-    #     first_name: 'First',
-    #     last_name: 'Last',
-    #     email: 'lighthout@lab.com',
-    #     password: nil,
-    #     password_confirmation: 'password'
-    #   )
-    #   expect(@user).to_not be_valid
-    # end
-
-    # xit 'should not be valid without a password confirmation' do
-    #   @user = User.new(
-    #     first_name: 'First',
-    #     last_name: 'Last',
-    #     email: 'lighthout@lab.com',
-    #     password: 'password',
-    #     password_confirmation: nil
-    #   )
-    #   expect(@user).to_not be_valid
-    # end
-
-    # xit 'should not be valid if a password conirmation is different from the password' do
-    #   @user = User.new(
-    #     first_name: 'First',
-    #     last_name: 'Last',
-    #     email: 'lighthout@lab.com',
-    #     password: 'password',
-    #     password_confirmation: 'different'
-    #   )
-    #   expect(@user).to_not be_valid
-    # end
-  # end
-# end
